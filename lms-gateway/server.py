4c613f18a3072b4d43a9c5b80854dbee5885adc1
simphiwe madi <42252666+mrbrogrammer@users.noreply.github.com>
2026-01-06 20:23:18 +0200

    Gateway inital commit

---
+++ lms-gateway/server.py
@@ -0,0 +1,35 @@
+import sys
+import time
+
+if __name__ == "__main__":
+    sys.stdout.write("Starting lms-gateway...")
+
+    while True:
+        sys.stdout.write('|')
+        sys.stdout.flush()
+
+        time.sleep(0.5)
+        
+        sys.stdout.write('\b \b')
+        sys.stdout.write('/')
+        sys.stdout.flush()
+        
+        time.sleep(0.5)
+
+        sys.stdout.write('\b \b')
+        sys.stdout.write('-')
+        sys.stdout.flush()
+        
+        time.sleep(0.5)
+    
+        sys.stdout.write('\b \b')
+        sys.stdout.write('\\')
+        sys.stdout.flush()
+
+        time.sleep(0.5)
+        
+        sys.stdout.write('\b \b')
+
+
+
+
