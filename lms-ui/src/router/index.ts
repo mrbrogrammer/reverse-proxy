822435aa6b4a72c9a1f6e259798cd434cbb9d047
simphiwe madi <42252666+mrbrogrammer@users.noreply.github.com>
2026-01-08 23:22:16 +0200

    Attempt to setup routes

--- lms-ui/src/router/index.ts
+++ lms-ui/src/router/index.ts
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Courses from '../Courses.vue'
+import App from '../App.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', component: App },
+    { path: '/courses', component: Courses }
+  ],
 })
 
 export default router
