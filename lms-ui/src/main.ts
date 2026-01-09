2ee26842a30ec9e1bf62619924e8ec59f4b18589
simphiwe madi <42252666+mrbrogrammer@users.noreply.github.com>
2026-01-09 21:47:34 +0200

    WIP

--- lms-ui/src/main.ts
+++ lms-ui/src/main.ts
@@ -1,10 +1,21 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createRouter, createWebHistory } from 'vue-router'
+import Courses from './Courses.vue'
 
 import App from './App.vue'
-import router from './router'
+// import router from './router/index.ts'
 
 const app = createApp(App)
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    // { path: '/', component: App },
+    { path: '/', name: 'courses', component: Courses },
+  ],
+})
+
 
 app.use(createPinia())
 app.use(router)
