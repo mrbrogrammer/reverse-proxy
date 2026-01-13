7de9cde3ca5b270111ae179cbce7f7d901a80750
simphiwe madi <42252666+mrbrogrammer@users.noreply.github.com>
2026-01-13 12:10:18 +0200

    Add lms-users db

---
+++ lms-users/config/connect.py
@@ -0,0 +1,15 @@
+from config import load_config
+import psycopg2
+
+def connect(config):
+    """ Connect to the PostgreSQL database server """
+    try:
+        # connecting to the PostgreSQL server
+        with psycopg2.connect(**config) as conn:
+            print('Connected to the PostgreSQL server.')
+            return conn
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+if __name__ == '__main__':
+    config = load_config()
+    connect(config)
