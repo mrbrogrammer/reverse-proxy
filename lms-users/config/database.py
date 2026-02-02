ad32a8e86c79340d06d831656fa195c9288cc759
simphiwe madi <42252666+mrbrogrammer@users.noreply.github.com>
2026-02-02 21:17:57 +0200

    Setup db

--- lms-users/config/database.py
+++ lms-users/config/database.py
@@ -1,7 +1,11 @@
 import os
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
@@ -9,19 +13,18 @@ load_dotenv()
 ASYNC_DATABASE_URL = os.getenv("ASYNC_DATABASE_URL")
 
 # Asynchronous engine
-engine = create_async_engine(ASYNC_DATABASE_URL, echo=True)
+engine = create_engine("postgresql://admin:$tar&B3y0nd@localhost:5432/lms-users-dev", echo=True)
+
+# postgresql://{username}:{password}@{host}:{port}/{database_name} 
 
 # Session maker
-AsyncSessionLocal = sessionmaker(
-    engine, 
-    class_=AsyncSession, 
-    expire_on_commit=False
-)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-
-async def get_async_db():
-    """ Dependency to get an async database session """
-    async with AsyncSessionLocal() as session:
-        yield session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
