39d443bc70b1fb9404de2dbf29858531ec2ea245
simphiwe madi <42252666+mrbrogrammer@users.noreply.github.com>
2026-02-02 21:19:34 +0200

    DBMS

--- lms-users/domain/dto/user_dto.py
+++ lms-users/domain/dto/user_dto.py
@@ -2,10 +2,11 @@ from pydantic import BaseModel
 from typing import List, Optional
 
 class UserDTO(BaseModel):
-    id: Optional[int] = None
-    name: str
-    email: str
+    uid: Optional[int] = None
+    first_name: str
+    last_name: str
 
     class Config:
         orm_mode = True
         allow_population_by_field_name = True
+
--- lms-users/domain/user.py
+++ lms-users/domain/user.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String
 from config.database import Base
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(50))
-    email = Column(String(100))
+
+    uid = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(50))
+    last_name = Column(String(100))
